e9fff728e27cc9a4e0f1c1315c534b7947860cc8
Haima <hymars@gmail.com>
2019-12-09 17:29:40 +0530

    first commit

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -14,61 +14,45 @@ import androidx.appcompat.app.AppCompatActivity;
 /* methods to invoke
 LoginActivity
  */
-public class LoginActivity extends AppCompatActivity
-{
+public class LoginActivity extends AppCompatActivity {
 
-    protected void onCreate(@Nullable Bundle x)
-    {
+    protected void onCreate(@Nullable Bundle x) {
         super.onCreate(x);
         setContentView(R.layout.activity_login);
-        final EditText etusername=findViewById(R.id.editText1);
-        final EditText etpassword=findViewById(R.id.editText2);
-        Button btnLogin=findViewById(R.id.btnLogin);
+        final EditText etusername = findViewById(R.id.editText1);
+        final EditText etpassword = findViewById(R.id.editText2);
+        Button btnLogin = findViewById(R.id.btnLogin);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String username=etusername.getText().toString();
-                String password=etpassword.getText().toString();
+                String username = etusername.getText().toString();
+                String password = etpassword.getText().toString();
 
-                if(username.equals("username")&& password.equals("password"))
-                {
-                   // Intent intent=new Intent(getApplicationContext(), W.class);
+                if (username.equals("username") && password.equals("password")) {
+                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
 
-                   //startActivity(intent);
+                    startActivity(intent);
 
 
-                }
-                else
-                {
-                    Toast.makeText(getApplicationContext(),"Invalid credentials",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Invalid credentials", Toast.LENGTH_LONG).show();
 
 
                 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
             }
         });
 
     }
+}
 
-    public void processSignupLinkClick(View view)
+/**    public void processSignupLinkClick(View view)
     {
-    // Intent intent=new Intent(this, RegistrationActivity.class);
-    // startActivity(intent);
+     Intent intent=new Intent(this, RegistrationActivity.class);
+     startActivity(intent);
 
     }
-}
+}**/
